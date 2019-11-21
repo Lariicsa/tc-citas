4019d607eb3c8bb15f41b5facb769caec61b7b2b
Larissa <dejamepienso@gmail.com>
2019-11-20 22:18:05 -0600

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="space" id="app">
    <Header></Header>
+   <div class="container">
+   </div>
+   <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'app',
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer>
+    <div class="container">
+      <div class="row md">
+        <p>Visita nuestras redes sociales</p>
+      </div>
+      <div class="row md">
+        <ul>
+          <li>fb</li>
+          <li>yt</li>
+          <li>ig</li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+@import "../scss/index.scss";
+
+footer {
+  display: flex;
+  height: 10vh;
+  background-color: $primary;
+  width: 100%;
+}
+</style>
