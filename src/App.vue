8fde9edece82aaf6e12b468c0c6b5cea2d7e595c
Larissa <dejamepienso@gmail.com>
2019-11-21 20:11:42 -0600

    Add form

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,62 @@
 <template>
   <div class="space" id="app">
-   <Header></Header>
-   <div class="container">
-     <Carousel />
-   </div>
-   <Footer></Footer>
+    <Header></Header>
+    <div class="container">
+      <div class="row center">
+        <h2>Visita sin costo las casa que más te gusten</h2>
+      </div>
+      <div class="row center">
+        <p>Elige la fecha y en qué momento del día:</p>
+      </div>
+      <div class="row">
+        <div class="radioGroup">
+          <ul>
+            <OptionButton name="options" label="Por la mañana" :value="selectedValue" @change="changeValue"/>
+            <OptionButton name="options" label="Por la tarde" :value="selectedValue" @change="changeValue"/>
+          </ul>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <SendForm />
+        </div>
+        <div class="row">
+          <p>Al agendar, estás aceptando nuestro <a href="https://www.tucanton.com/avisoPrivacidad">Aviso de privacidad</a></p>
+        </div>
+      </div>
+      <Carousel />
+    </div>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
-import Carousel from './components/Carousel.vue'
-import Header from './components/Header.vue'
-import Footer from './components/Footer.vue'
+import Carousel from "./components/Carousel.vue";
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+import SendForm from "./components/Form.vue";
+import OptionButton from "./components/OptionButton";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Carousel,
     Header,
-    Footer
+    Footer,
+    SendForm,
+    OptionButton
+  },
+  data() {
+    return {
+      selectedValue: "1"
+    };
+  },
+  methods: {
+    changeValue: function(newValue) {
+      this.selectedValue = newValue;
+    }
   }
-}
+};
 </script>
 
 <style lang="scss">
---
+++ src/components/Form.vue
@@ -0,0 +1,31 @@
+<template>
+  <form class="form">
+    <div class="row">
+      <label>Nombre</label>
+      <input class="input" type="text" v-model="name" />
+    </div>
+    <div class="row">
+      <label>Teléfono</label>
+      <input type="tel" v-model="phone" />
+    </div>
+    <div class="row">
+      <label>Correo electrónico</label>
+      <input type="email" v-model="mail" />
+    </div>
+    <div class="row">
+      <button class="button" type="submit">Agendar visita</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+      phone: '',
+      mail: ''
+    }
+  }
+}
+</script>
